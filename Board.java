8adc631c16ec3334547c398fe8e154280e1272d6
elise <36968521+elisezhg@users.noreply.github.com>
2020-03-10 22:06:09 -0400

    optimizations
    use PriorityQueue
    check if the puzzle is solvable
    use manhattan distance instead of misplaced tiles

---
+++ Board.java
@@ -0,0 +1,136 @@
+package nouveau;
+
+import java.util.ArrayList;
+
+public class Board {
+    public int x, y;
+    public int previousX = -1, previousY = -1;
+    public int f, g = 0;
+    public Board parent = null;
+    public char[][] puzzle;
+    public Board goal;
+
+    public Board(int i, int j) {
+        puzzle = new char[i][j];
+    }
+
+    public void setChar(int i, int j, char newChar) {
+        puzzle[i][j] = newChar;
+        if (newChar == '_') {
+            x = i;
+            y = j;
+        }
+    }
+
+
+    // revoir pour n'import quel autre goal
+    public int computeH() {
+        int h = 0;
+        for (int i = 0; i < puzzle.length; i++) {
+            for (int j = 0; j < puzzle[0].length; j++) {
+                if(puzzle[i][j] == '1') h+=(Math.abs(i-0) + Math.abs(j-0));
+                if(puzzle[i][j] == '2') h+=(Math.abs(i-0) + Math.abs(j-1));
+                if(puzzle[i][j] == '3') h+=(Math.abs(i-0) + Math.abs(j-2));
+                if(puzzle[i][j] == '8') h+=(Math.abs(i-1) + Math.abs(j-0));
+                if(puzzle[i][j] == '0') h+=(Math.abs(i-1) + Math.abs(j-1));
+                if(puzzle[i][j] == '4') h+=(Math.abs(i-1) + Math.abs(j-2));
+                if(puzzle[i][j] == '7') h+=(Math.abs(i-2) + Math.abs(j-0));
+                if(puzzle[i][j] == '6') h+=(Math.abs(i-2) + Math.abs(j-1));
+                if(puzzle[i][j] == '5') h+=(Math.abs(i-2) + Math.abs(j-2));
+            }
+        }
+
+        return h;
+    }
+
+    public int f() {
+        return computeH() + g;
+    }
+
+
+    public ArrayList<Board> getChildren() {
+        ArrayList<Board> children = new ArrayList<Board>(0);
+        if (x-1 >=0 && (x-1 != previousX || y != previousY)) {
+            children.add(this.exchange(x-1, y));
+        }
+
+        if (x+1 < 3 && (x+1 != previousX || y != previousY)) {
+            children.add(this.exchange(x+1, y));
+        }
+
+        if (y-1 >=0 && (x != previousX || y-1 != previousY)) {
+            children.add(this.exchange(x, y-1));
+        }
+
+        if (y+1 < 3 && (x != previousX || y+1 != previousY)) {
+            children.add(this.exchange(x, y+1));
+        }
+
+        return children;
+    }
+
+    /*public void setParent(Board parent) {
+        this.parent = parent;
+    }*/
+
+    public Board exchange(int x2, int y2) {
+        Board copie = new Board(3,3);
+        for (int i = 0; i < puzzle.length; i++) {
+            for (int j = 0; j < puzzle[0].length; j++) {
+                copie.puzzle[i][j] = puzzle[i][j];
+            }
+        }
+
+        copie.puzzle[x][y] = puzzle[x2][y2];
+        copie.puzzle[x2][y2] = '_';
+        copie.x = x2;
+        copie.y = y2;
+        copie.previousX = x;
+        copie.previousY = y;
+        copie.g = g + 1;
+        return copie;
+    }
+
+
+    @Override
+    public String toString() {
+        String str = "";
+        for (int i = 0; i < puzzle.length; i++) {
+            str += puzzle[i][0] + " " + puzzle[i][1] + " " + puzzle[i][2] + "\n";
+        }
+
+        return str;
+    }
+
+    public boolean isSolvable() {
+        int inversions = 0;
+        for (int i = 0; i < puzzle.length; i++) {
+            for (int j = 0; j < puzzle[0].length; j++) {
+                inversions += inversionsCase(puzzle[i][j], 3*i + j);
+            }
+        }
+
+        return (inversions % 2 == 0) ? true : false;
+    }
+
+
+    /**
+     * Check nombre d'inversions d'une case
+     */
+    public int inversionsCase(char car, int pos) {
+
+        if (car == '_') return 0;
+
+        char[] cases = { goal.puzzle[0][0], goal.puzzle[0][1], goal.puzzle[0][2],
+                         goal.puzzle[1][0], goal.puzzle[1][1], goal.puzzle[1][2],
+                         goal.puzzle[2][0], goal.puzzle[2][1], goal.puzzle[2][2] };
+
+        int inversions = 0;
+
+        for (int i = pos; i < cases.length; i++) {
+            if (cases[i] != '_' && car > cases[i]) inversions++;
+        }
+
+        return inversions;
+    }
+}
--- Solver.java
+++ Solver.java
@@ -1,106 +1,85 @@
+package nouveau;
+
+import java.util.*;
 import java.util.Scanner;
-import java.lang.*;
 
 public class Solver {
+
+    public static int moves = 0;
+
+    public static void reconstitutePath(Board board) {
+        if (board.parent == null) {
+            System.out.println("----------------");
+            System.out.println(board);
+        } else {
+            moves++;
+            reconstitutePath(board.parent);
+            System.out.println("\n  ||  \n  \\/\n");
+            System.out.println(board);
+        }
+    }
+
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Enter initial setup:");
-        Puzzle init = new Puzzle(3,3);
-        for(int i = 0; i < 3; i++) {
-            for(int j = 0; j < 3; j++) {
-                init.setChar(i, j, scan.next().charAt(0));
-            }
-        }
-
         System.out.println("Enter goal:");
-        Puzzle goal = new Puzzle(3,3);
+        Board goal = new Board(3,3);
         for(int i = 0; i < 3; i++) {
             for(int j = 0; j < 3; j++) {
                 goal.setChar(i, j, scan.next().charAt(0));
             }
         }
 
-        List closedList = new List(0);
-        List openList = new List(0);
-        openList.add(init);
-
-        while (openList.list.length != 0) {
-            openList.sort();
-
-            Puzzle current = openList.depiler();
-
-            if (current.equals(goal.puzzle)) {
-                current.path();
-                goal.afficher();
-                System.exit(0);
+        System.out.println("Enter initial setup:");
+        Board init = new Board(3,3);
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                init.setChar(i, j, scan.next().charAt(0));
             }
-
-            int x = current.getX();
-            int y = current.getY();
-            int prevX = current.previousX;
-            int prevY = current.previousY;
-
-            // liste des voisins
-            List voisins = new List(0);
-            if (x-1 >=0 && (x-1 != prevX || y != prevY)) {
-                voisins.add(current.exchange(x-1, y));
-            }
-
-            if (x+1 < 3 && (x+1 != prevX || y != prevY)) {
-                voisins.add(current.exchange(x+1, y));
-            }
-
-            if (y-1 >=0 && (x != prevX || y-1 != prevY)) {
-                voisins.add(current.exchange(x, y-1));
-            }
-
-            if (y+1 < 3 && (x != prevX || y+1 != prevY)) {
-                voisins.add(current.exchange(x, y+1));
-            }
-
-
-            for (int i = 0; i < voisins.list.length; i++) {
-                Puzzle voisin = voisins.list[i];
-                voisin.parent = current;
-
-                voisin.g = current.g + 1;
-                int h = voisin.computeH(goal.puzzle);
-                voisin.f = voisin.g + h;
-
-                boolean coutInf = true;
-
-                if (openList.includes(voisin)) {
-                    for (int j = 0; j < openList.list.length; j++) {
-                        if (openList.list[j].equals(voisin.puzzle)) {
-                            if(openList.list[j].f <= voisin.f) {
-                                coutInf = false;
-                            }
-                        }
-                    }
-                }
-
-                boolean coutInf2 = true;
-
-                if (closedList.includes(voisin)) {
-                    for (int j = 0; j < closedList.list.length; j++) {
-                        if (closedList.list[j].equals(voisin.puzzle)) {
-                            if(closedList.list[j].f <= voisin.f) {
-                                coutInf2 = false;
-                            }
-                        }
-                    }
-                }
-                if (coutInf2 && coutInf) {
-                    openList.add(voisin);
-
-                }
-            }
-
-            closedList.add(current);
         }
 
-        System.out.println("erreur");
+        scan.close();
+
+        init.goal = goal;
+
+        if (!init.isSolvable()) {
+            System.out.println("ERREUR : PAS DE SOLUTION");
+            System.exit(0);
+        }
+
+        PriorityQueue<Board> openList = new PriorityQueue<Board>((a,b) -> a.f() - b.f());
+        ArrayList<char[][]> closedList = new ArrayList<char[][]> (0);
+
+        openList.add(init);
+
+        while (openList.size() != 0) {
+            Board current = openList.poll();
+
+            if (Arrays.deepEquals(current.puzzle, goal.puzzle)) {
+                reconstitutePath(current);
+                System.out.println("It took " + moves + " moves!");
+                return;
+            }
+
+            ArrayList<Board> children = current.getChildren();
+
+            for (int i = 0; i < children.size(); i++) {
+
+                Board child = children.get(i);
+                child.goal = goal;
+                child.parent = current;
+
+                if (!closedList.contains(child.puzzle)) {
+                    child.g = current.g + 1;
+                    openList.add(child);
+                }
+            }
+
+            closedList.add(current.puzzle);
+
+        }
+
     }
+
 }
